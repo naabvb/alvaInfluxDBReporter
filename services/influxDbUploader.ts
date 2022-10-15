42b4bfa78294a7b68f4e76c2b1398ac6117542df
naabvb <46524999+naabvb@users.noreply.github.com>
2022-10-15 14:28:42 +0300

    Add timestamp shifting

--- services/influxDbUploader.ts
+++ services/influxDbUploader.ts
@@ -1,4 +1,5 @@
 import * as Influx from 'influx';
+import { DateTime } from 'luxon';
 import { InfluxDbSettings } from '../interfaces/InfluxDb';
 
 export class InfluxDbUploader {
@@ -8,6 +9,11 @@ export class InfluxDbUploader {
     this.client = this.createClient(settings);
   }
 
+  /**
+   * Creates client for influxDB.
+   * @param settings Settings for influxDB connection built from environment variables and/or defaults
+   * @returns InfluxDB client
+   */
   createClient = (settings: InfluxDbSettings): Influx.InfluxDB => {
     return new Influx.InfluxDB({
       host: settings.host,
@@ -28,11 +34,25 @@ export class InfluxDbUploader {
     });
   };
 
+  /**
+   * Calculates and corrects timestamp to wanted timezone. Necessary due to Oomi incorrectly reporting UTC+2/UTC+3 timestamps as UTC.
+   * @param timestamp Timestamp in milliseconds
+   * @returns Shifted timestamp in milliseconds
+   */
+  shiftTimestamp = (timestamp: number) => {
+    const tzDifferenceInMinutes = DateTime.now().setZone(process.env.TZ).offset - DateTime.now().setZone('UTC').offset;
+    return DateTime.fromMillis(timestamp).minus({ minutes: tzDifferenceInMinutes }).toMillis();
+  };
+
+  /**
+   * Writes data points to influxDB. Uses measurement "consumption" by default.
+   * @param consumptionPairs Array of [Timestamp, kWh] tuples
+   */
   writeDataPoints = async (consumptionPairs: [number, number][]): Promise<void> => {
     const points = consumptionPairs.map(([timestamp, kwh]) => ({
       measurement: 'consumption',
       fields: { kwh: kwh },
-      timestamp: new Date(timestamp),
+      timestamp: new Date(this.shiftTimestamp(timestamp)),
     }));
     return await this.client.writePoints(points);
   };
